af977d899a78cfeae1bed3a92e8514779ad9ac02
Sara Eduarda da Silva <saradudasiilva@gmail.com>
2025-08-21 21:30:40 -0300

    First Commit

---
+++ src/test/java/projeto/anotacoes/pratica1/Pratica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.anotacoes.pratica1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
